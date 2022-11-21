0d75c6a34e36fd07fbc6bab5eff6b070205650eb
Avril Maleham <avrilmaleham@outlook.com>
2022-11-22 12:50:30 +1300

    22Nov22-1

--- mod1Practice/mod1PracticeTest.js
+++ mod1Practice/mod1PracticeTest.js
@@ -1,26 +1,26 @@
-var Sentiment = require ('sentiment');
+var Sentiment = require("sentiment");
 var sentiment = new Sentiment();
-var result = sentiment.analyze('Cats are stupid.');
-console.dir(result);    // Score: -2, Comparative: -0.666
+var result = sentiment.analyze("Cats are stupid.");
+console.dir(result); // Score: -2, Comparative: -0.666
 
-function add (a,b) {
-    return a + b
-};
-console.log(add(1,2));
+function add(a, b) {
+  return a + b;
+}
+console.log(add(1, 2));
 
-function sub (a,b) {
-    return a - b
-};
-console.log(sub(1,2));
+function sub(a, b) {
+  return a - b;
+}
+console.log(sub(1, 2));
 
-function mul (a,b) {
-    return a * b
-};
-console.log(mul(1,2));
+function mul(a, b) {
+  return a * b;
+}
+console.log(mul(1, 2));
 
-function div (a,b) {
-    return a / b
-};
-console.log(div(1,2));
+function div(a, b) {
+  return a / b;
+}
+console.log(div(1, 2));
 
-console.log("hello")
+console.log("hello");
